407472b925b864d553afa916ba7a11d48e403565
blockchainz <37352972+blockchainz@users.noreply.github.com>
2019-03-13 09:41:58 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -218,7 +218,7 @@ var TxtType = function(el, toRotate, period) {
         //converting the string into an array of single characters
         chinese = chinese.split("");
 
-        var font_size = 9;
+        var font_size = 14;
         var columns = c.width/font_size; //number of columns for the rain
         //an array of drops - one per column
         var drops = [];
@@ -256,3 +256,55 @@ var TxtType = function(el, toRotate, period) {
         }
 
         setInterval(draw, 33);
+
+
+
+        /*
+        Reference: http://jsfiddle.net/BB3JK/47/
+        */
+
+        $('select').each(function(){
+            var $this = $(this), numberOfOptions = $(this).children('option').length;
+
+            $this.addClass('select-hidden');
+            $this.wrap('<div class="select"></div>');
+            $this.after('<div class="select-styled"></div>');
+
+            var $styledSelect = $this.next('div.select-styled');
+            $styledSelect.text($this.children('option').eq(0).text());
+
+            var $list = $('<ul />', {
+                'class': 'select-options'
+            }).insertAfter($styledSelect);
+
+            for (var i = 0; i < numberOfOptions; i++) {
+                $('<li />', {
+                    text: $this.children('option').eq(i).text(),
+                    rel: $this.children('option').eq(i).val()
+                }).appendTo($list);
+            }
+
+            var $listItems = $list.children('li');
+
+            $styledSelect.click(function(e) {
+                e.stopPropagation();
+                $('div.select-styled.active').not(this).each(function(){
+                    $(this).removeClass('active').next('ul.select-options').hide();
+                });
+                $(this).toggleClass('active').next('ul.select-options').toggle();
+            });
+
+            $listItems.click(function(e) {
+                e.stopPropagation();
+                $styledSelect.text($(this).text()).removeClass('active');
+                $this.val($(this).attr('rel'));
+                $list.hide();
+                //console.log($this.val());
+            });
+
+            $(document).click(function() {
+                $styledSelect.removeClass('active');
+                $list.hide();
+            });
+
+        });
